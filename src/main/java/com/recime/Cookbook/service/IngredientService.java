cb92169e96067cc44450f95768ab73af2ff0bf04
lucasgandac <lucasgandac@hotmail.com>
2025-06-22 19:48:49 -0300

    feat: add service layer

--- src/main/java/com/recime/Cookbook/service/IngredientService.java
+++ src/main/java/com/recime/Cookbook/service/IngredientService.java
@@ -1,4 +1,26 @@
 package com.recime.Cookbook.service;
 
+import com.recime.Cookbook.model.Ingredient;
+import com.recime.Cookbook.repository.IngredientRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class IngredientService {
+    private final IngredientRepo ingredientRepo;
+
+    public IngredientService(IngredientRepo ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    public Ingredient findOrSaveByName(String ingredientName){
+        return ingredientRepo.findByName(ingredientName).orElseGet(() ->
+                ingredientRepo.save(new Ingredient(null, ingredientName))
+        );
+    }
+
+    public Optional<Ingredient> findByName(String ingredientName){
+        return ingredientRepo.findByName(ingredientName);
+    }
 }
--- src/main/java/com/recime/Cookbook/service/RecipeService.java
+++ src/main/java/com/recime/Cookbook/service/RecipeService.java
@@ -1,4 +1,43 @@
 package com.recime.Cookbook.service;
 
+import com.recime.Cookbook.model.Ingredient;
+import com.recime.Cookbook.model.Recipe;
+import com.recime.Cookbook.repository.RecipeRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class RecipeService {
+    private final RecipeRepo recipeRepo;
+    private final IngredientService ingredientService;
+
+    public RecipeService(RecipeRepo recipeRepo, IngredientService ingredientService) {
+        this.recipeRepo = recipeRepo;
+        this.ingredientService = ingredientService;
+    }
+
+    public List<Recipe> getAllRecipes(){
+        return recipeRepo.findAll();
+    }
+
+    public Optional<Recipe> getRecipeById(Long recipeId){
+        return recipeRepo.findById(recipeId);
+    }
+
+    public void deleteRecipe(Long recipeId){
+        recipeRepo.deleteById(recipeId);
+    }
+
+    public Recipe saveRecipe(Recipe recipe){
+        List<Ingredient> processedIngredients = recipe.getIngredients().stream()
+                        .map(ingredient -> ingredientService.findOrSaveByName(ingredient.getName()))
+                .collect(Collectors.toList());
+
+        recipe.setIngredients(processedIngredients);
+        return recipeRepo.save(recipe);
+    }
+
 }
